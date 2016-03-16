25c6fa465479a336228a74dff0dd4d1aa202f231
MariaDaniela <maria.henrique@ccc.ufcg.edu.br>
2016-03-16 00:25:48 -0300

    Editando e removendo usando um modal

--- livraria.js
+++ livraria.js
@@ -6,6 +6,20 @@
 		$scope.index = 0;
 		$scope.book = db[$scope.index];
 		
+	
+		$scope.addComment = function(comment) {
+			$scope.book.comments.push(comment);
+		};
+		
+		$scope.addBook = function(mtitle, mauthors, mdescription, mphoto, mprice, mcomments) {
+			db.push({title: mtitle, authors: mauthors, description: mdescription, photo: mphoto, price: mprice, comments: mcomments});
+		
+		};
+		
+		$scope.editBook = function(mtitle, mauthors, mdescription, mphoto, mprice) {
+			$scope.book = {title: mtitle, authors: mauthors, description: mdescription, photo: mphoto, price: mprice, comments: $scope.book.comments};
+		};
+		
 		$scope.next = function() {
 			$scope.index = $scope.index == db.length - 1 ? 0 : $scope.index + 1;
 			$scope.book = db[$scope.index];		
@@ -15,17 +29,10 @@
 			$scope.book = db[$scope.index];
 		};
 		
-		$scope.addComment = function(comment) {
-			$scope.book.comments.push(comment);
-		};
-		
-		$scope.addBook = function(mtitle, mauthors, mdescription, mphoto, mprice, mcomments) {
-			db.push({title: mtitle, authors: mauthors, description: mdescription, photo: mphoto, price: mprice, comments: mcomments});
-		
-		}
-		
-		
+	
 		
 	});	
 
+
 })();
+
